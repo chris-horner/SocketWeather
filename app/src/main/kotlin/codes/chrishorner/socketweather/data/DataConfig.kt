f6d213ba0dd0af98b2469a1eef0a8d554c1d879c
Christopher Horner <horner.cd@gmail.com>
2019-12-23 23:31:46 +1100

    Add json adapters for time types used in API

--- app/src/main/kotlin/codes/chrishorner/socketweather/data/DataConfig.kt
+++ app/src/main/kotlin/codes/chrishorner/socketweather/data/DataConfig.kt
@@ -1,13 +1,29 @@
 package codes.chrishorner.socketweather.data
 
+import com.squareup.moshi.FromJson
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.ToJson
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.threeten.bp.Instant
+import org.threeten.bp.ZoneId
 
 object DataConfig {
 
   const val API_ENDPOINT = "https://api.weather.bom.gov.au/v1/"
 
   val moshi: Moshi = Moshi.Builder()
+      .add(InstantAdapter)
+      .add(ZoneIdAdapter)
       .add(KotlinJsonAdapterFactory())
       .build()
+
+  private object InstantAdapter {
+    @ToJson fun toJson(value: Instant) = value.toString()
+    @FromJson fun fromJson(value: String): Instant = Instant.parse(value)
+  }
+
+  private object ZoneIdAdapter {
+    @ToJson fun toJson(value: ZoneId) = value.toString()
+    @FromJson fun fromJson(value: String): ZoneId = ZoneId.of(value)
+  }
 }

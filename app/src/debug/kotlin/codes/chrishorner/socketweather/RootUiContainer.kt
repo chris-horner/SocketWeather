546e9fdb7c92cee27cb1f20d9ba394b72773e43b
Chris Horner <chris@horner.chat>
2021-04-10 11:05:42 +1000

    Use systemuicontroller from accompanist

--- app/src/debug/kotlin/codes/chrishorner/socketweather/RootUiContainer.kt
+++ app/src/debug/kotlin/codes/chrishorner/socketweather/RootUiContainer.kt
@@ -2,21 +2,30 @@ package codes.chrishorner.socketweather
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import codes.chrishorner.socketweather.debug.ComposeDebugDrawer
 import codes.chrishorner.socketweather.styles.SocketWeatherTheme
 import com.google.accompanist.insets.ExperimentalAnimatedInsets
 import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberAndroidSystemUiController
 
 @Composable
 @ExperimentalAnimatedInsets
 fun RootContainer(content: @Composable () -> Unit) {
-  ProvideWindowInsets(windowInsetsAnimationsEnabled = true) {
-    SocketWeatherTheme {
+  SocketWeatherTheme {
+    ProvideWindowInsets(windowInsetsAnimationsEnabled = true) {
+      val systemUiController = rememberAndroidSystemUiController()
+      val useDarkIcons = MaterialTheme.colors.isLight
+      SideEffect {
+        systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = useDarkIcons)
+      }
       ComposeDebugDrawer {
         Box(modifier = Modifier.fillMaxSize()) {
-         content()
+          content()
         }
       }
     }

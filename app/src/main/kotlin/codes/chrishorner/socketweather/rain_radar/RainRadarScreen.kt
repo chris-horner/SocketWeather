8e83a650241a5775d9448bb3774f689b5a955aa6
Chris Horner <chris@horner.chat>
2021-08-16 23:20:04 +1000

    Refactor map code, add loading indicator

--- app/src/main/kotlin/codes/chrishorner/socketweather/rain_radar/RainRadarScreen.kt
+++ app/src/main/kotlin/codes/chrishorner/socketweather/rain_radar/RainRadarScreen.kt
@@ -1,55 +1,75 @@
 package codes.chrishorner.socketweather.rain_radar
 
-import android.content.Context
-import android.graphics.Color
-import android.preference.PreferenceManager
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.Lifecycle.Event.ON_DESTROY
-import androidx.lifecycle.Lifecycle.Event.ON_PAUSE
-import androidx.lifecycle.Lifecycle.Event.ON_RESUME
-import androidx.lifecycle.LifecycleEventObserver
-import codes.chrishorner.socketweather.R
-import codes.chrishorner.socketweather.data.generateRainRadarTimestamps
-import codes.chrishorner.socketweather.util.allowMainThreadDiskOperations
-import kotlinx.coroutines.Dispatchers
+import com.google.accompanist.insets.systemBarsPadding
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import org.osmdroid.config.Configuration
-import org.osmdroid.tileprovider.MapTileProviderBase
-import org.osmdroid.tileprovider.MapTileProviderBasic
-import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
-import org.osmdroid.tileprovider.tilesource.TileSourcePolicy
 import org.osmdroid.util.GeoPoint
-import org.osmdroid.util.MapTileIndex.getX
-import org.osmdroid.util.MapTileIndex.getY
-import org.osmdroid.util.MapTileIndex.getZoom
-import org.osmdroid.views.CustomZoomButtonsController.Visibility
-import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.TilesOverlay
 
 @Composable
 fun RainRadarScreen() {
-  val rawMapView = rememberMapViewWithLifecycle()
+  val systemUiController = rememberSystemUiController()
+  val useDarkIcons = MaterialTheme.colors.isLight
 
-  val context = LocalContext.current
-  val overlays = remember {
-    generateRainRadarTimestamps().map { timestamp -> getRainOverlay(context, timestamp) }
+  // Force dark system icons while viewing Rain Radar Screen.
+  DisposableEffect(Unit) {
+    systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = true)
+    onDispose {
+      systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = useDarkIcons)
+    }
   }
+
+  var loading by remember { mutableStateOf(true) }
+
+  Box {
+    RainRadar { loading = it }
+    AnimatedVisibility(
+      visible = loading,
+      Modifier.align(Alignment.TopEnd),
+      enter = fadeIn(),
+      exit = fadeOut(),
+    ) {
+      CircularProgressIndicator(
+        modifier = Modifier
+          .systemBarsPadding()
+          .padding(16.dp)
+      )
+    }
+  }
+}
+
+@Composable
+private fun RainRadar(setLoading: (Boolean) -> Unit) {
+  val rawMapView = rememberMapViewWithLifecycle()
+  val context = LocalContext.current
   val scope = rememberCoroutineScope()
+  val overlays = remember { getRainRadarOverlays(context) }
 
   AndroidView({ rawMapView }) { mapView ->
 
     mapView.tileProvider = getTileProvider(context)
     mapView.controller.setZoom(9.0)
     mapView.controller.setCenter(GeoPoint(-37.80517674019138, 144.98394260916697))
-
     mapView.overlays.addAll(overlays)
 
     scope.launch {
@@ -60,12 +80,13 @@ fun RainRadarScreen() {
         val overlay = overlays[index]
         overlay.isEnabled = true
         mapView.invalidate()
+        setLoading(overlays.any { it.tileStates.notFound > 0 })
 
-        if (overlay.tileStates.isDone) {
+        if (overlay.tileStates.notFound == 0) {
           // Pause on each overlay for 500ms, or 1s if it's the last.
           if (index == overlays.size - 1) delay(1_000) else delay(500)
         } else {
-          while (!overlay.tileStates.isDone) {
+          while (overlay.tileStates.notFound > 0) {
             delay(500)
           }
         }
@@ -75,98 +96,5 @@ fun RainRadarScreen() {
         if (index >= overlays.size) index = 0
       }
     }
-  }
-}
-
-private fun getTileProvider(context: Context): MapTileProviderBase {
-  val source = object : OnlineTileSourceBase(
-    "Terrain",
-    0,
-    18,
-    256,
-    "@2x.png",
-    arrayOf(
-      "https://stamen-tiles-a.a.ssl.fastly.net/terrain",
-      "https://stamen-tiles-b.a.ssl.fastly.net/terrain",
-      "https://stamen-tiles-c.a.ssl.fastly.net/terrain",
-      "https://stamen-tiles-d.a.ssl.fastly.net/terrain",
-    ),
-    "© OpenStreetMap contributors",
-    TileSourcePolicy(4, 0)
-  ) {
-    override fun getTileURLString(pMapTileIndex: Long): String {
-      return "$baseUrl/${getZoom(pMapTileIndex)}/${getX(pMapTileIndex)}/${getY(pMapTileIndex)}$mImageFilenameEnding"
-    }
-  }
-
-  return MapTileProviderBasic(context, source)
-}
-
-private fun getRainOverlay(context: Context, timestamp: String): TilesOverlay {
-  val source = object : OnlineTileSourceBase(
-    timestamp,
-    3,
-    10,
-    256,
-    ".png",
-    arrayOf("https://api.weather.bom.gov.au/v1/rainradar/tiles/$timestamp"),
-    "© Australian Bureau of Meteorology",
-    RadarTileSourcePolicy
-  ) {
-    override fun getTileURLString(pMapTileIndex: Long): String {
-      return "$baseUrl/${getZoom(pMapTileIndex)}/${getX(pMapTileIndex)}/${getY(pMapTileIndex)}$mImageFilenameEnding"
-    }
-  }
-  val provider = MapTileProviderBasic(context, source)
-  val overlay = TilesOverlay(provider, context)
-  overlay.loadingBackgroundColor = Color.TRANSPARENT
-  overlay.isEnabled = false
-  return overlay
-}
-
-@Composable
-private fun rememberMapViewWithLifecycle(): MapView {
-  val context = LocalContext.current
-  val scope = rememberCoroutineScope()
-  LaunchedEffect(Unit) {
-    scope.launch(Dispatchers.IO) {
-      // This API isn't going anywhere, and it's not worth pulling in the AndroidX version.
-      @Suppress("DEPRECATION")
-      Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context))
-    }
-  }
-
-  val mapView = remember {
-    // MapView performs disk operations on the main thread when initialised 😢.
-    allowMainThreadDiskOperations {
-      MapView(context).apply {
-        id = R.id.map
-        setMultiTouchControls(true)
-        zoomController.setVisibility(Visibility.NEVER)
-        isTilesScaledToDpi = true
-      }
-    }
-  }
-
-  val lifecycle = LocalLifecycleOwner.current.lifecycle
-  DisposableEffect(lifecycle, mapView) {
-    val lifecycleObserver = getMapLifecycleObserver(mapView)
-    lifecycle.addObserver(lifecycleObserver)
-    onDispose {
-      lifecycle.removeObserver(lifecycleObserver)
-    }
-  }
-
-  return mapView
-}
-
-private fun getMapLifecycleObserver(
-  mapView: MapView
-): LifecycleEventObserver = LifecycleEventObserver { _, event ->
-  @Suppress("NON_EXHAUSTIVE_WHEN") // osmdroid only cares about these events.
-  when (event) {
-    ON_RESUME -> mapView.onResume()
-    ON_PAUSE -> mapView.onPause()
-    ON_DESTROY -> mapView.onDetach()
   }
 }

5797f20d0fb5c78994d6a39651a6045d0a3ab33c
Chris Horner <chris@horner.chat>
2022-01-02 12:05:45 +1100

    Play around with Glance modifiers

--- app/src/main/kotlin/codes/chrishorner/socketweather/widget/ForecastWidget.kt
+++ app/src/main/kotlin/codes/chrishorner/socketweather/widget/ForecastWidget.kt
@@ -1,8 +1,16 @@
 package codes.chrishorner.socketweather.widget
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.appwidget.appWidgetBackground
+import androidx.glance.appwidget.background
+import androidx.glance.appwidget.cornerRadius
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
 import androidx.glance.text.Text
 import codes.chrishorner.socketweather.data.Forecast
 
@@ -14,6 +22,14 @@ class ForecastWidget(forecast: Forecast?) : GlanceAppWidget() {
 
   @Composable
   override fun Content() {
-    Text("A compose widget!")
+    Text(
+      text = "A compose widget!",
+      modifier = GlanceModifier
+        .fillMaxSize()
+        .background(day = Color.White, night = Color.DarkGray)
+        .appWidgetBackground()
+        .cornerRadius(16.dp)
+        .padding(8.dp)
+    )
   }
 }

6e79593391fa4ea8f5378faafe56b3db71137eb0
Chris Horner <chris@horner.chat>
2022-02-19 18:28:10 +1100

    Create compose util

--- app/src/main/kotlin/codes/chrishorner/socketweather/util/Compose.kt
+++ app/src/main/kotlin/codes/chrishorner/socketweather/util/Compose.kt
@@ -1,50 +1,25 @@
 package codes.chrishorner.socketweather.util
 
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.contentColorFor
-import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import com.google.accompanist.insets.navigationBarsPadding
-import com.google.accompanist.insets.statusBarsPadding
+import androidx.compose.runtime.LaunchedEffect
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
 
 /**
- * A wrapper around [TopAppBar] which uses [Modifier.statusBarsPadding] to shift the app bar's
- * contents down, but still draws the background behind the status bar too.
- *
- * Thanks Chris: https://github.com/chrisbanes/accompanist/blob/main/sample/src/main/java/dev/chrisbanes/accompanist/sample/insets/EdgeToEdgeLazyColumn.kt
+ * Like
+ * ```
+ * LaunchedEffect(flow) {
+ *   flow.collect { }
+ * }
+ * ```
+ * but forces the collecting code to handle emissions without suspending.
  */
 @Composable
-fun InsetAwareTopAppBar(
-  title: @Composable () -> Unit,
-  modifier: Modifier = Modifier,
-  navigationIcon: @Composable (() -> Unit)? = null,
-  actions: @Composable RowScope.() -> Unit = {},
-  backgroundColor: Color = MaterialTheme.colors.primarySurface,
-  contentColor: Color = contentColorFor(backgroundColor),
-  elevation: Dp = 4.dp
-) {
-  Surface(
-    color = backgroundColor,
-    elevation = elevation,
-    modifier = modifier
-  ) {
-    TopAppBar(
-      title = title,
-      navigationIcon = navigationIcon,
-      actions = actions,
-      backgroundColor = Color.Transparent,
-      contentColor = contentColor,
-      elevation = 0.dp,
-      modifier = Modifier
-        .statusBarsPadding()
-        .navigationBarsPadding(start = true, end = true, bottom = false)
-    )
+inline fun <T> CollectEffect(flow: Flow<T>, crossinline block: CoroutineScope.(T) -> Unit) {
+  LaunchedEffect(flow) {
+    coroutineScope {
+      flow.collect { block(it) }
+    }
   }
 }

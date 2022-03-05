f7bca390b5afe25ad609dbd777087f2cc01c2f3f
Chris Horner <chris@horner.chat>
2022-03-05 17:56:41 +1100

    Remove unused utils

--- app/src/test/kotlin/codes/chrishorner/socketweather/test/TestUtils.kt
+++ app/src/test/kotlin/codes/chrishorner/socketweather/test/TestUtils.kt
@@ -4,44 +4,13 @@ import app.cash.turbine.FlowTurbine
 import app.cash.turbine.test
 import com.google.common.truth.Subject
 import com.google.common.truth.Truth.assertThat
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.test.TestCoroutineScheduler
-import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
-import kotlinx.coroutines.test.runBlockingTest
 import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.contract
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.coroutines.coroutineContext
-
-/**
- * The same as [runBlockingTest], except the [TestCoroutineScope] is cancelled at the end of
- * `testBody`'s execution, along with any coroutines that are still running.
- *
- * The key difference is that `runBlockingTest` throws an exception if it finishes
- * with coroutines running, where as this function simply cancels them.
- *
- * This is useful when testing code that subscribes to unending streams (such as a [SharedFlow]),
- * and rather than orchestrating cancellation from that source manually you simply want the end
- * of the test to cancel that subscription.
- *
- * @see runBlockingTest
- */
-fun runCancellingBlockingTest(
-  context: CoroutineContext = EmptyCoroutineContext,
-  testBody: suspend TestCoroutineScope.() -> Unit
-) = runBlockingTest(context) {
-  val job = Job()
-  val scope = TestCoroutineScope(coroutineContext + job)
-  testBody(scope)
-  scope.advanceUntilIdle()
-  job.cancel()
-}
 
 suspend fun <T> Flow<T>.testWithScheduler(validate: suspend FlowTurbine<T>.() -> Unit) {
   val testScheduler = coroutineContext[TestCoroutineScheduler]

cbc709266a7289164c1faad8e9464a0ab9f49088
Chris Horner <chris@horner.chat>
2022-03-12 08:55:39 +1100

    Remove dependency on appcompat

--- app/src/main/kotlin/codes/chrishorner/socketweather/MainActivity.kt
+++ app/src/main/kotlin/codes/chrishorner/socketweather/MainActivity.kt
@@ -1,11 +1,11 @@
 package codes.chrishorner.socketweather
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)

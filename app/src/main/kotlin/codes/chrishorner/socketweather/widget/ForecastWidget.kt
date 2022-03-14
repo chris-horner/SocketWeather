f64b1e7dd31e05f24d1019fdefda791a3d8ae397
Chris Horner <chris@horner.chat>
2022-03-14 21:17:42 +1100

    Add row widget layout

--- app/src/main/kotlin/codes/chrishorner/socketweather/widget/ForecastWidget.kt
+++ app/src/main/kotlin/codes/chrishorner/socketweather/widget/ForecastWidget.kt
@@ -48,11 +48,12 @@ class ForecastWidget : GlanceAppWidget() {
   companion object {
     private val TINY_BOX = DpSize(48.dp, 48.dp)
     private val TINY_ROW = DpSize(96.dp, 48.dp)
-    private val SMALL_ROW = DpSize(160.dp, 48.dp)
+    private val SMALL_ROW = DpSize(144.dp, 48.dp)
+    private val ROW = DpSize(220.dp, 48.dp)
   }
 
   override val sizeMode = SizeMode.Responsive(
-    setOf(TINY_BOX, TINY_ROW, SMALL_ROW)
+    setOf(TINY_BOX, TINY_ROW, SMALL_ROW, ROW)
   )
 
   @SuppressLint("StateFlowValueCalledInComposition") // Get the current forecast once per invalidation.
@@ -65,6 +66,7 @@ class ForecastWidget : GlanceAppWidget() {
       TINY_BOX -> TinyBox(forecast)
       TINY_ROW -> TinyRow(forecast)
       SMALL_ROW -> SmallRow(forecast)
+      ROW -> Row(forecast)
     }
   }
 }
@@ -168,6 +170,69 @@ private fun SmallRow(forecast: Forecast?) {
       Spacer(modifier = GlanceModifier.defaultWeight())
       Text(
         text = strings.get(R.string.widget_feels, strings.formatDegrees(forecast?.tempFeelsLike?.roundToInt())),
+        maxLines = 1,
+        style = TextStyle(fontSize = 14.sp),
+      )
+    }
+    Spacer(modifier = GlanceModifier.defaultWeight())
+    Column(
+      horizontalAlignment = Alignment.CenterHorizontally,
+      modifier = GlanceModifier.fillMaxHeight(),
+    ) {
+      Text(
+        text = strings.formatDegrees(forecast?.highTemp),
+        maxLines = 1,
+        style = TextStyle(fontSize = 14.sp, fontWeight = FontWeight.Medium),
+        modifier = GlanceModifier.padding(top = 6.dp),
+      )
+      Image(
+        provider = ImageProvider(R.drawable.bg_line),
+        contentDescription = null,
+        modifier = GlanceModifier.width(4.dp).defaultWeight().padding(vertical = 6.dp),
+      )
+      Text(
+        text = strings.formatDegrees(forecast?.lowTemp),
+        maxLines = 1,
+        style = TextStyle(fontSize = 14.sp, fontWeight = FontWeight.Medium),
+      )
+    }
+  }
+}
+
+@Composable
+private fun Row(forecast: Forecast?) {
+  val strings = AndroidStrings(LocalContext.current)
+  val iconRes = weatherIconRes(forecast?.iconDescriptor, night = forecast?.night ?: false)
+
+  Row(
+    modifier = parentModifier.padding(8.dp),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalAlignment = Alignment.CenterVertically,
+  ) {
+    Image(
+      provider = ImageProvider(iconRes),
+      contentDescription = strings[R.string.widget_description],
+      modifier = GlanceModifier.width(48.dp).fillMaxHeight(),
+    )
+    Spacer(modifier = GlanceModifier.width(24.dp))
+    Column(
+      modifier = GlanceModifier.fillMaxHeight(),
+      verticalAlignment = Alignment.CenterVertically,
+    ) {
+      Row {
+        Text(
+          text = strings.formatDegrees(forecast?.currentTemp?.roundToInt()),
+          maxLines = 1,
+          style = TextStyle(fontSize = 36.sp, fontWeight = FontWeight.Medium),
+        )
+        Text(
+          text = strings.get(R.string.widget_feels, strings.formatDegrees(forecast?.tempFeelsLike?.roundToInt())),
+          maxLines = 1,
+          style = TextStyle(fontSize = 14.sp),
+        )
+      }
+      Text(
+        text = forecast?.todayForecast?.short_text ?: "",
         maxLines = 1,
         style = TextStyle(fontSize = 14.sp),
       )
